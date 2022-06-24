3b5693dd8ba6f725544ebdaf1048af5de1081a0a
Jaime Cesar Carlos Lemes Filho <101151851+JaimeCCLF@users.noreply.github.com>
2022-06-24 14:46:14 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,35 @@
+// ------------- Exemplo -------------
+// function somar(numero1, numero2) {
+//     let soma = numero1 + numero2;
+//     return 'A soma dos dois números é:' + soma;
+// }
+
+// -------------- SOMA --------------
+function soma(numero1, numero2){
+    return numero1 + numero2
+}
+
+let resultadoDaSoma = soma(5,7)
+console.log(resultadoDaSoma)
+
+// -------------- DIVISÂO --------------
+function divisao(numero1, numero2){
+    return numero1 / numero2
+}
+let resultadoDaDivisao = divisao(20, 5)
+console.log(resultadoDaDivisao)
+
+// -------------- MULTIPLICAÇÃO --------------
+function multiplicacao(numero1, numero2){
+    return numero1 * numero2
+}
+let resultadoDaMultiplicacao = multiplicacao(8, 6)
+console.log(resultadoDaMultiplicacao)
+
+// -------------- SUBTRAÇÃO --------------
+function subtracao(numero1, numero2){
+    return numero1 - numero2
+}
+
+let resultadoDaSubtracao = subtracao(12,4)
+console.log(resultadoDaSubtracao)
